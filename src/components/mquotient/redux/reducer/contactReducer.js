092445940d5e528d6d8c06fec172e86ebb837bb8
Vinay325 <vinay.s325@gmail.com>
2021-01-03 07:36:42 +0530

    contact book

--- src/components/mquotient/redux/reducer/contactReducer.js
+++ src/components/mquotient/redux/reducer/contactReducer.js
@@ -3,43 +3,43 @@ import { CREATE_CONTACT, GET_CONTACT, UPDATE_CONTACT, DELETE_CONTACT } from '../
 const initialState = {
     contactsList: [
         {
-            "id": 1,
+            "id": "1",
             "name": "Vinay Kumar",
             "email": "testvinay@test.com",
             "phone": "11x21xxx111"
         },
         {
-            "id": 2,
+            "id": "2",
             "name": "Rahul BK",
             "email": "rahulbk@test.com",
             "phone": "999999xx01"
         },
         {
-            "id": 3,
+            "id": "3",
             "name": "Vipin",
             "email": "vipin@test.com",
             "phone": "7899x000xx"
         },
         {
-            "id": 4,
+            "id": "4",
             "name": "Tuhina",
             "email": "tuhina@kory.org",
             "phone": "001110001212"
         },
         {
-            "id": 5,
+            "id": "5",
             "name": "Divya",
             "email": "divyaa@kory.org",
             "phone": "49179623x156"
         },
         {
-            "id": 6,
+            "id": "6",
             "name": "Vino",
             "email": "vino@kory.com",
             "phone": "293xxx11101"
         },
         {
-            "id": 7,
+            "id": "7",
             "name": "Sunil",
             "email": "sunil@test.com",
             "phone": "8x8998x1111"
@@ -56,7 +56,7 @@ export const contactReducer = (state = initialState, action) => {
                 contactsList: [action.payload, ...state.contactsList]
             };
         case GET_CONTACT:
-            let arr = state.contactsList.filter((contact) => contact.id == action.payload);
+            let arr = state.contactsList.filter((contact) => contact.id === action.payload);
             arr = arr.values()
             for (let val of arr) {
                 arr = val
